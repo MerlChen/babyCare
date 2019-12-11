68c05cfa11b455642dea585638e0c5f33dafa989
Louis <huangxin1273@vip.qq.com>
2019-12-11 23:25:21 +0800

    【增加收藏和阅读数显示功能】

--- src/pages/article/details.vue
+++ src/pages/article/details.vue
@@ -11,43 +11,81 @@
       v-html="dataInfo.content"
     >
     </div>
+    <div class="article-like-info">
+      <div class="article-like-item">
+        <img src="./../../static/article/view.png" alt="">
+        <div class="label">
+          {{ dataInfo.viewsNum }}
+        </div>
+      </div>
+      <div class="article-like-item">
+        <img
+          src="./../../static/article/favorite.png"
+          alt=""
+          v-if="!favorited"
+          @click="favoriteArticle"
+        >
+        <img
+          src="./../../static/article/favorited.png"
+          v-if="favorited"
+          alt=""
+          @click="cancelFavorite"
+        >
+        <div class="label">
+          {{ favorited ? '取消收藏' : '收藏本文' }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "details",
-  data() {
-    return {
-      dataInfo: {}
+  export default {
+    name: "details",
+    data() {
+      return {
+        dataInfo: {},
+        favorited: false
+      }
+    },
+    methods: {
+      /**
+       * @description 根据文章ID获取文章详情
+       * @param articleInfo
+       */
+      async getArticleDetails(articleInfo) {
+        let result = await this.$ajax.post("/api/article/details", articleInfo)
+        let d = new Date(result.createTime);
+        result.createTime = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+        result.content = result.content.replace(/\<img/gi, "<img class='content-img'")
+        this.dataInfo = result;
+        uni.setNavigationBarTitle({
+          title: result.title
+        })
+      },
+      /**
+       * @description 收藏该文章
+       */
+      favoriteArticle() {
+        this.favorited = true;
+      },
+      /**
+       * @description 取消收藏
+       */
+      cancelFavorite() {
+        this.favorited = false;
+      }
+    },
+    onLoad: function (options) {
+      this.getArticleDetails(options)
+    },
+    onPullDownRefresh() {
+      setTimeout(() => {
+        uni.stopPullDownRefresh();
+        this.getArticleDetails(true);
+      }, 1000);
     }
-  },
-  methods: {
-    /**
-     * @description 根据文章ID获取文章详情
-     * @param articleInfo
-     */
-    async getArticleDetails(articleInfo) {
-      let result = await this.$ajax.post("/api/article/details", articleInfo)
-      let d = new Date(result.createTime);
-      result.createTime = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
-      result.content = result.content.replace(/\<img/gi,"<img class='content-img'")
-      this.dataInfo = result;
-      uni.setNavigationBarTitle({
-        title: result.title
-      })
-    }
-  },
-  onLoad: function (options) {
-    this.getArticleDetails(options)
-  },
-  onPullDownRefresh() {
-    setTimeout(() => {
-      uni.stopPullDownRefresh();
-      this.getArticleDetails(true);
-    }, 1000);
   }
-}
 </script>
 
 <style lang="scss">
@@ -76,6 +114,25 @@ export default {
 
       .content-img {
         width: 100% !important;
+      }
+    }
+
+    .article-like-info {
+      padding: 40rpx 40rpx 40rpx;
+      display: flex;
+      justify-content: space-between;
+      .article-like-item{
+        display: flex;
+        justify-content: flex-start;
+        .label{
+          font-size: 28rpx;
+          line-height: 46rpx;
+        }
+      }
+      img{
+        width: 46rpx;
+        height: 46rpx;
+        margin-right: 10rpx;
       }
     }
   }
