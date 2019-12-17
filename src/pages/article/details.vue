bebcdf43db87ad2fee111cae5b61f7dcb9cf113b
Louis <huangxing@enfry.com>
2019-12-17 11:25:04 +0800

    【增加留言模块】

--- src/pages/article/details.vue
+++ src/pages/article/details.vue
@@ -1,39 +1,74 @@
 <template>
   <div class="article-details">
-    <div class="article-title">
-      {{ dataInfo.title }}
-    </div>
-    <div class="article-time">
-      {{ dataInfo.createTime }}
-    </div>
-    <div
-      class="article-content"
-      v-html="dataInfo.content"
-    >
-    </div>
-    <div class="article-like-info">
-      <div class="article-like-item">
-        <img src="./../../static/article/view.png" alt="">
-        <div class="label">
-          {{ dataInfo.viewsNum }}
-        </div>
+    <div class="article-main">
+      <div class="article-title">
+        {{ dataInfo.title }}
+      </div>
+      <div class="article-time">
+        {{ dataInfo.createTime }}
       </div>
       <div
-        class="article-like-item"
-        @click="favoriteEvent"
+        class="article-content"
+        v-html="dataInfo.content"
       >
-        <img
-          src="./../../static/article/favorite.png"
-          alt=""
-          v-if="!favorite"
+      </div>
+      <!-- 收藏、查看次数 -->
+      <div class="article-like-info">
+        <div class="article-like-item">
+          <img src="./../../static/article/view.png" alt="">
+          <div class="label">
+            {{ dataInfo.viewsNum }}
+          </div>
+        </div>
+        <div
+          class="article-like-item"
+          @click="favoriteEvent"
         >
-        <img
-          src="./../../static/article/favorited.png"
-          v-if="favorite"
-          alt=""
+          <img
+            src="./../../static/article/favorite.png"
+            alt=""
+            v-if="!favorite"
+          >
+          <img
+            src="./../../static/article/favorited.png"
+            v-if="favorite"
+            alt=""
+          >
+          <div class="label">
+            {{ favorite ? '取消收藏' : '收藏本文' }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 文章评论 -->
+    <div class="article-comment">
+      <!-- 评论输入框 -->
+      <div class="article-comment-title">
+        <div class="title">
+          <img src="./../../static/article/favorited.png" alt="">
+          精选留言
+        </div>
+        <div class="btn" @click="writeMessage">
+          写留言
+        </div>
+      </div>
+      <!-- 评论列表 -->
+      <div class="article-comment-list">
+        <!-- 单条评论 -->
+        <div
+          v-for="item in commentList"
+          :key="item.id"
+          class="article-comment-item"
         >
-        <div class="label">
-          {{ favorite ? '取消收藏' : '收藏本文' }}
+          <div class="comment-item-user-info">
+            <img :src="item.avatarUrl" alt="" class="comment-item-header">
+            <div class="comment-item-name">
+              {{ item.nickName }}
+            </div>
+          </div>
+          <div class="comment-item-content">
+            {{ item.content }}
+          </div>
         </div>
       </div>
     </div>
@@ -46,7 +81,8 @@ export default {
   data() {
     return {
       dataInfo: {},
-      favorite: false
+      favorite: false,
+      commentList: []
     }
   },
   methods: {
@@ -64,6 +100,7 @@ export default {
       result.createTime = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
       result.content = result.content.replace(/\<img/gi, "<img class='content-img'")
       this.dataInfo = result;
+      this.getCommentList();
       uni.setNavigationBarTitle({
         title: result.title
       })
@@ -106,6 +143,20 @@ export default {
         })
         this.favorite = false;
       }
+    },
+    /**
+     * @description 获取评论列表
+     */
+    async getCommentList() {
+      this.commentList = await this.$ajax.post("/api/article/getCommentList",{
+        id: this.dataInfo.id
+      })
+    },
+    /**
+     * @description 写留言
+     */
+    writeMessage(){
+
     }
   },
   onLoad: function (options) {
@@ -132,52 +183,126 @@ export default {
 
 <style lang="scss">
   .article-details {
-    background-color: #ffffff;
-    padding: 20rpx 20rpx 20rpx;
+    .article-main {
+      background-color: #ffffff;
+      padding: 20rpx 20rpx 20rpx;
 
-    .article-title {
-      text-align: center;
-      font-size: 30rpx;
-      font-weight: bold;
-      color: #333333;
-    }
-
-    .article-time {
-      text-align: center;
-      font-size: 24rpx;
-      line-height: 44rpx;
-      color: #666666;
-    }
-
-    .article-content {
-      width: 710rpx;
-      padding-bottom: 20rpx;
-      color: #333333;
-
-      .content-img {
-        width: 100% !important;
+      .article-title {
+        text-align: center;
+        font-size: 30rpx;
+        font-weight: bold;
+        color: #333333;
       }
-    }
 
-    .article-like-info {
-      padding: 40rpx 40rpx 40rpx;
-      display: flex;
-      justify-content: space-between;
+      .article-time {
+        text-align: center;
+        font-size: 24rpx;
+        line-height: 44rpx;
+        color: #666666;
+      }
 
-      .article-like-item {
-        display: flex;
-        justify-content: flex-start;
+      .article-content {
+        width: 710rpx;
+        padding-bottom: 20rpx;
+        color: #333333;
 
-        .label {
-          font-size: 28rpx;
-          line-height: 46rpx;
+        .content-img {
+          width: 100% !important;
         }
       }
 
-      img {
-        width: 46rpx;
-        height: 46rpx;
-        margin-right: 10rpx;
+      .article-like-info {
+        padding: 40rpx 40rpx 40rpx;
+        display: flex;
+        justify-content: space-between;
+
+        .article-like-item {
+          display: flex;
+          justify-content: flex-start;
+
+          .label {
+            font-size: 28rpx;
+            line-height: 46rpx;
+          }
+        }
+
+        img {
+          width: 46rpx;
+          height: 46rpx;
+          margin-right: 10rpx;
+        }
+      }
+    }
+
+    .article-comment {
+      margin-top: 20rpx;
+      background-color: #ffffff;
+      padding: 30rpx 20rpx 20rpx;
+
+      .article-comment-title {
+        display: flex;
+        padding: 0 20rpx 30rpx;
+        justify-content: space-between;
+        font-size: 30rpx;
+        line-height: 40rpx;
+        margin-bottom: 10rpx;
+        color: #666666;
+        .title{
+          display: flex;
+          justify-content: flex-start;
+          img{
+            width: 40rpx;
+            height: 40rpx;
+            margin-right: 10rpx;
+          }
+        }
+        .btn{
+          color: #007aff;
+        }
+      }
+
+      .article-comment-list {
+        .article-comment-item {
+          margin-bottom: 40rpx;
+          display: flex;
+          justify-content: flex-start;
+
+          &:nth-last-child(1) {
+            margin: 0;
+          }
+
+          .comment-item-user-info {
+            width: 80rpx;
+            text-align: center;
+
+            .comment-item-header {
+              width: 80rpx;
+              height: 80rpx;
+              border-radius: 20rpx 20rpx 20rpx;
+              float: left;
+            }
+
+            .comment-item-name {
+              float: left;
+              width: 100%;
+              font-size: 20rpx;
+              line-height: 20rpx;
+              margin-top: 10rpx;
+              color: #666666;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+          }
+
+          .comment-item-content {
+            margin: 0 0 0 20rpx;
+            font-size: 24rpx;
+            color: #333333;
+            line-height: 30rpx;
+            width: calc(100% - 120rpx);
+          }
+        }
       }
     }
   }
